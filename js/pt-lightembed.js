2db1ab78f673c65a43d54c907f7b2b46ae65ac43
spunce <spunce>
2026-01-13 21:52:16 +0100

    0

--- js/pt-lightembed.js
+++ js/pt-lightembed.js
@@ -21,7 +21,7 @@
 
   function labnolIframe() {
     var iframe = document.createElement("iframe");
-     var embed = "https://video.sadmin.io/videos/embed/ID?autoplay=1";
+     var embed = "https://www.kotikoff.net/videos/embed/ID?autoplay=1";
     // var embed = "https://mega.nz/embed/ID";
     iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
     //iframe.setAttribute("sandbox", "allow-same-origin allow-scripts allow-popups");
